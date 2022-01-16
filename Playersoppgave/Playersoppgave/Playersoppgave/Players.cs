6e358036871af0a26c42ac2bf0eff67ce967699f
JanneCathrin <77385680+JanneCathrin@users.noreply.github.com>
2022-01-17 00:17:27 +0100

    Add files via upload

---
+++ Playersoppgave/Playersoppgave/Playersoppgave/Players.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playersoppgave
+{
+    class Player
+    {
+        public Random RandomGen;
+        private string _name;
+        private int _points;
+
+
+        public Player(string name, int points)
+        {
+            RandomGen = new Random();
+            _name = name;
+            _points = points;
+
+        }
+        public void Play(Player player2)
+        {
+            var winner = RandomGen.Next(2) == 0 ? this : player2;
+            var loser = winner == this ? player2 : this;
+            winner._points += 1;
+            loser._points -= 1;
+
+        }
+
+        public void ShowNameAndPoints()
+        {
+            Console.WriteLine($"name {_name}. {_points}");
+        }
+       
+    }
+}
---
+++ Playersoppgave/Playersoppgave/Playersoppgave/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Playersoppgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+
+            
+            var players = new[]
+            { 
+                new Player("Per", 10), 
+                new Player("Pål", 10), 
+                new Player("Espen", 10) 
+            
+            };
+
+            for (var round = 1; round <= 10; round++)
+            {
+                var playerIndex1 = players[0].RandomGen.Next(players.Length);
+                var playerIndex2 = (playerIndex1 + 1 + players[1].RandomGen.Next(2)) % players.Length;
+                var player1 = players[playerIndex1];
+                var player2 = players[playerIndex2];
+                player1.Play(player2);
+            }
+
+            foreach (var player in players)
+            {
+                player.ShowNameAndPoints();
+            }
+
+        }
+    }
+}
